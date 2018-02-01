dd45ec1cb0f4f0403b1e1255325106d4bfaa33e3
lanakirayuu <lanakirayuu@10.211.55.7>
2018-02-01 17:41:39 +0800

    测试1741

--- src/com/yirong/Test.java
+++ src/com/yirong/Test.java
@@ -1,10 +1,15 @@
 package com.yirong;
 
+import org.omg.CORBA.PRIVATE_MEMBER;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("测试");
+		System.out.println("测试2222");
+		
 
 	}
 
