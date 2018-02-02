2d5856859557aa67ed8fae441a24733de4e8f933
lanzy8 <wolflan@126.com>
2018-02-02 11:18:20 +0800

    133123123

--- src/com/yirong/Test.java
+++ src/com/yirong/Test.java
@@ -12,22 +12,8 @@ public class Test {
 		System.out.println("33333");
 		System.out.println("44444");
 		
-		System.out.println("您好呀亲爱额小西路同学你们你在在哪里呢");
-		//最后一次测试
-		System.out.println("999999");
-
-		//测试
-
-		你在哪撒
-		你几点下班
-		马上下班
+	
 		
-		
-		
-
-
-		哈哈
-
 
 
 		
